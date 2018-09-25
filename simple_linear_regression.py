2013a6a8a87817a5d049767d3f8e36a01cfe0fdf
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-09-25 23:04:04 +0530

    Chapter 2 Simple linear regression
    Udemy

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,6 +1,3 @@
-# Simple Linear Regression
-
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,19 +5,12 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
-☻
+X = dataset.iloc[:, :-1].values   # Except Dependednt so -1
+y = dataset.iloc[:, 1].values      # Independent data column
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
 
 # Fitting Simple linear regression in the training set
@@ -30,3 +20,19 @@ regressor.fit(X_train,y_train)
 
 # Predict the test result
 y_pred=regressor.predict(X_test)
+
+# Visualising training sets
+plt.scatter(X_train,y_train,color='red')     # Read data plot
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('years of experience')
+plt.ylabel('salary')
+plt.show()
+
+# Visualising test sets
+plt.scatter(X_test,y_test,color='red')     # Read data plot
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('years of experience')
+plt.ylabel('salary')
+plt.show()
