36b9577d7a2c819f9d46140c0c02e37b6008f9b7
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-09-25 14:05:17 +0530

    Chapter 1 Data Preprocessing
    Udemy

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -25,21 +25,21 @@ labelencoder_X=LabelEncoder()          #Country
 X[:,0]=labelencoder_X.fit_transform(X[:,0])                                    # it will give 0,1,2 has no significance
 
 ####Dummy encoding ##
-onehotencoder=OneHotEncoder(categorical_features = [0])                          # First county column
+onehotencoder=OneHotEncoder(categorical_features = [0])                        # First county column
 X=onehotencoder.fit_transform(X).toarray()
 
-'''
+labelencoder_y=LabelEncoder()
+y=labelencoder_y.fit_transform(y)                                              # Dependent data i.e. purchase category
 
-# Splitting the dataset into the Training set and Test set
+
+# 1.5 Spitting dataset into training set and test set
+
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,
+                                               random_state=0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
-'''
+# 1.6 Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
