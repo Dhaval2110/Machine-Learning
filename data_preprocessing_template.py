949420b2c4dc988a581bc75c2eefd05761aa391f
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2018-09-25 14:02:24 +0530

    Chapter 1 Data Preprocessing
    ML Chapter 1 - Udemy

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -20,7 +20,7 @@ X[:,1:3]=imputer.transform(X[:,1:3])
 # By example we can see county and purchased are categorial data
 
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncode
+from sklearn.preprocessing import OneHotEncoder
 labelencoder_X=LabelEncoder()          #Country
 X[:,0]=labelencoder_X.fit_transform(X[:,0])                                    # it will give 0,1,2 has no significance
 
@@ -28,17 +28,18 @@ X[:,0]=labelencoder_X.fit_transform(X[:,0])                                    #
 onehotencoder=OneHotEncoder(categorical_features = [0])                          # First county column
 X=onehotencoder.fit_transform(X).toarray()
 
+'''
 
-"""
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+'''
