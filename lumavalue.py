feda24c3b7575beab83dedbb963bbbac412936bc
Dhaval <32478060+Dhaval2110@users.noreply.github.com>
2019-04-20 23:08:05 +0530

    LumaValue.py

--- lumavalue.py
+++ lumavalue.py
@@ -1,14 +1,36 @@
 # Luma value calculation in python
 
-from PIL import Image 
-imag = Image.open(r"C:\Users\DHAVAL\Desktop\DSC08361.JPG")
-#Convert the image te RGB if it is a .gif for example
-imag = imag.convert ('RGB')
-#coordinates of the pixel
-X,Y = 0,0
-#Get RGB
-pixelRGB = imag.getpixel((X,Y))
-R,G,B = pixelRGB 
-luma = 0.2126*R + 0.7152*G + 0.0722*B
-#brightness = sum([R,G,B])/3 
-print(luma,"Luma")
+# Global Imports
+import os
+import xlwt 
+
+# Local Imports
+from PIL import Image
+from xlwt import Workbook 
+
+os.chdir(r'C:\Users\DHAVAL\Desktop\Luma4')                        # Changing directory to folder itself
+for filename in os.listdir(os.getcwd()):                          # List out all the files to run one by one
+	#print(filename)
+	imag = Image.open(filename)
+	#Convert the image te RGB if it is a .gif for example
+	imag = imag.convert ('RGB')
+	#coordinates of the pixel
+	X,Y = 0,0
+	#Get RGB
+	pixelRGB = imag.getpixel((X,Y))
+	R,G,B = pixelRGB 
+	luma = 0.2126*R + 0.7152*G + 0.0722*B
+	#brightness = sum([R,G,B])/3 
+	print(luma,"Luma")
+	 
+  ######## NEED TO WORK HERE MORE ###########
+	# # Workbook is created 
+	# wb = Workbook()   
+	# # add_sheet is used to create sheet. 
+	# sheet1 = wb.add_sheet('Sheet 1')   
+	# sheet1.write(1, 0, filename) 
+	# sheet1.write(1, 1, luma) 
+	# wb.save('xlwt_example.xls')
+	with open('out.txt', 'w') as f:
+			print(luma, filename,file=f)
+		
